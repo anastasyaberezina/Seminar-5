bdea455f0e9251fc65c42e13629930fa1e40bac8
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-06 11:29:05 +0300

    массив из 12 элементов от -9 до 9, заполненный случ числами с нахождением суммы отриц и полож чисел

---
+++ Seminar 5.1/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задайте массив из 12 элементов, заполненный случ 
+числами из промежутка [-9, 9]. Найдите сумму отриц и полож элементов массива.*/
+
+int[] array = GetArray (12, -9, 9);
+Console.WriteLine(String.Join(" ", array));
+
+int positiveSum = 0;
+int negativeSum = 0;
+for(int i = 0; i<array.Length; i++)
+{
+    if(array[i]<0)
+    {
+        negativeSum+=array[i];
+    }
+    else
+    {
+        positiveSum+=array[i];
+    }
+}
+
+Console.WriteLine($"Сумма положительных элементов массива {positiveSum}, сумма отрицательных элементов массива {negativeSum}");
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for(int i=0; i<size; i++)
+    {
+        res[i]= new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
