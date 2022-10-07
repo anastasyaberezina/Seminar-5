f15570cd19b0c3064a1621ecae6c5fd5858ab4b7
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-07 11:56:17 +0300

    определяет, присутствует ли заданное число в массиве

---
+++ Seminar 5.3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив. Напишите программу, которая определяет присутствует ли заданное число в массиве.
+
+Console.WriteLine("Введите число: ");
+int[] array = MyArray(9, 1, 99);
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine(String.Join(" ", array));
+int result1 = 0;
+int result2 = 0;
+
+for(int a = 0; a<array.Length; a++)
+{
+if(array[a]!=x)
+{
+    result1=array[a];
+    Console.WriteLine($"Заданного числа в массиве НЕТ!");
+}
+else
+{
+    result2 = array[a];
+    Console.WriteLine($"Заданное число {result2} присутствует в массиве.");
+}
+}
+
+int[] MyArray (int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
