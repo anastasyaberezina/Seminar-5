3ba86598bf853b22d67a13ddfc1edc9a84220f0b
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-08 22:02:14 +0300

    Найдите произведение пар чисел в одномерном массиве.

--- Seminar 5.5/Program.cs
+++ Seminar 5.5/Program.cs
@@ -1,13 +1,14 @@
 ﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-//Не работает, почему не могу разобраться.
+//Не могу разобраться, чтото делаю не то, или возвращаю не то.
 
-int[] array = MyArray(5, 1, 9);
-Console.WriteLine(String.Join(" ", array));
+int[] array = MyArray(5, 1, 9); //Задали массив
+Console.WriteLine(String.Join(" ", array)); //Считали массив
 int[] result = new int[array];
 Console.WriteLine(String.Join(" ", result));
-int Size= 0;
 
-if(array.Length %2==0)
+int Size = 0; //Половинчатый размер массива
+
+if(array.Length%2==0)
 {
     Size = array.Langth/2;
 }
@@ -16,28 +17,32 @@ else
     Size = array.Length/2+1;
 }
 
-for(int i = 0; y=array.Length-1; i<Size, i++, y--)
+
+for(int i = 0; int y = 0; y=array.Length-1; i<Size, i++, y--) //Чтото не выходит, чтото не то возвращаю.
 {
     array[x] = array[i];
     array[y] = array[y];
-    
+     
     if(i!=y)
     {
         array[i] = array[x]*array[y];
+        result = array[i];
     }
     else
     {
         array[i]=array[y];
+        result = array[i];
     }
-    return array[i];
+    return result;
 }
 
-int[] MyArray (int size, int a, int b)
+int[] MyArray(int size, int minValue, int maxValue)
 {
     int[] res = new int[size];
-    for(int i = 0; i<size; i++)
+
+    for(int i=0; i<size; i++)
     {
-        res[i] = new Random().Next(a, b+1);
+        res[i]= new Random().Next(minValue, maxValue+1);
     }
     return res;
 }
