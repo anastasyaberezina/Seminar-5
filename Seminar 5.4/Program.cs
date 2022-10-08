979da5e9c8e57fd333192549611e8fcef8f685f6
Anastasya <gavrilovaanastasya@yandex.ru>
2022-10-08 20:58:20 +0300

    массив из 123 случ чисел и определить кол-во элементов, лежащих в отрезке от 10 до 99

--- Seminar 5.4/Program.cs
+++ Seminar 5.4/Program.cs
@@ -1,20 +1,24 @@
 ﻿// Задать одномерный массив из 123 случ чисел и определить кол-во элементов, лежащих в отрезке от 10 до 99 включительно.
+//Не вполне уверена, что решила правильно, потому что не пойму в отрезке между элементами со значениями 10 и 99 или индексами?
 
-int[] array = MyArray(123, 0, 150);
-Console.WriteLine(String.Join(" ", array));
-int result = 0;
-int x = 0;
+int[] array = MyArray(123, 0, 150); //задали массив
+Console.WriteLine(String.Join(" ", array)); //считали массив
+
+int count = 0;
+int min = array[9]; //10й элемент массива
+int max = array[98]; //99 элемент массива
+int result = 0;  //кол-во элементов в отрезке
 
 for(int i = 0; i<array.Length; i++)
 {
-    array[x] = array[i];
-    
-    if((array[x]>=array[9])&&(array[x]<=array[98]))
-    {
-        result = array[i];
-        Console.Write($"[{String.Join(";", result)}]");
+    if((min<=array[i])&&(array[i]<=max))
+    { 
+        result=array[i];
+        count++;
     }
 }
+    Console.WriteLine($"Количество элементов в отрезке{count}");
+
 
 int[] MyArray (int size, int a, int b)
 {
